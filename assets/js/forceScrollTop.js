8e7ae7f05865757e2314fd15464c887dcbbf2b2d
Igor Marcondes <igormarcondesdasilva@gmail.com>
2024-11-12 22:18:38 -0300

    Atualizacao de script

--- assets/js/forceScrollTop.js
+++ assets/js/forceScrollTop.js
@@ -1,12 +1,14 @@
-    // Garante que a página carregue no topo em todos os dispositivos
+    // Garante que a página carregue no topo e desativa a restauração de rolagem
+    window.history.scrollRestoration = 'manual'; // Desativa a restauração automática da rolagem
+
+    // Força a rolagem para o topo no carregamento
     window.addEventListener('load', function() {
         setTimeout(function() {
             window.scrollTo(0, 0);
-        }, 0); // Atraso mínimo para garantir que o scroll é aplicado após o carregamento
+        }, 0);
     });
 
-    // Força a rolagem para o topo ao recarregar a página
+    // Garante que a página role para o topo ao recarregar ou sair da página
     window.addEventListener('beforeunload', function() {
         window.scrollTo(0, 0);
     });
-
